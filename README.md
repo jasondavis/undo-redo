5286c8aaba96969d84cd757315f2ae42272d7362
Epistemex <github@epistemex.com>
2016-01-01 17:28:48 -0600

    See change.log

---
+++ README.md
@@ -0,0 +1,91 @@
+﻿undo-redo stack
+===============
+
+**undo-redo** is an all-purpose lightweight undo/redo stack which you
+can store any kind of custom data to.
+
+**undo-redo** comes with simple methods to undo, redo, get status (for
+buttons), do stack dump (for storage) and more.
+
+**undo-redo** will take care of all the logistics for you.
+
+
+Features
+--------
+
+- Lightweight (< 1 kb) and efficient
+- Add any kind of data to the stack (text, images, canvas data, objects, arrays etc.).
+- Single undo() call, returns object for new state
+- Single redo() call, returns object for new state (if possible)
+- Can use callback functions on undo/redo
+- Set optional limit (with fifo)
+- Get undo/redo status, count and pointer
+- Get current stack so it can be stored persistently.
+- HTML documentation included in addition to source documentation
+
+See the `tests` directory for example usage.
+
+
+Install
+-------
+
+**undo-redo** can be installed in various ways:
+
+- Bower: `bower install undo-redo`
+- Git using HTTPS: `git clone https://github.com/epistemex/undo-redo.git`
+- Git using SSH: `git clone git@github.com:epistemex/undo-redo.git`
+- Download [zip archive](https://github.com/epistemex/undo-redo/archive/master.zip) and extract.
+- [undo-redo.js](https://raw.githubusercontent.com/epistemex/undo-redo/master/src/undo-redo.js)
+- [undo-redo.min.js](https://raw.githubusercontent.com/epistemex/undo-redo/master/undo-redo.min.js)
+
+
+Usage
+-----
+
+Include the script in your project, then create an instance:
+
+    var stack = new UndoRedo();
+
+At this point we can start adding states to the stack:
+
+    stack.add(myData);               // push data to stack, each add is one undo state
+
+To undo, simply call the undo() method. Data from the previous state is returned.
+If at first entry null will be returned as there won't be a previous state.
+
+    var data = stack.undo();         // return previous state data
+    if (data) ...set data...         // see tests directory for sample usage
+
+If no `add()` was called since last undo, you can call `redo()`. The data for
+new current state is returned, or null if a redo was not possible.
+
+    var data = stack.redo();         // redo and return data for next state if any
+    if (data) ...set data...
+
+Or using callbacks:
+
+    var stack = new UndoRedo();
+    stack.onundo = function(data) { ... };	// get data or null
+    stack.onredo = function(data) { ... };	// get data or null
+    ...
+    stack.undo();                           // invokes callback
+
+or by options:
+
+    var stack = new UndoRedo({
+		limit: 100,                         // defaults to -1 = unlimited
+	    onUndo: undoCallback,
+	    onRedo: redoCallback
+    });
+
+See the `tests` directory for example usage.
+
+
+License
+-------
+
+Released under [MIT license](http://choosealicense.com/licenses/mit/). You may use this class in both commercial and non-commercial projects provided that full header (minified and developer versions) is included.
+
+*&copy; 2015-2016 Epistemex*
+
+![Epistemex](http://i.imgur.com/wZSsyt8.png)
---
+++ src/undo-redo.js
@@ -0,0 +1,211 @@
+/*!
+undo-redo v. 1.2.1
+(c) Epistemex 2015-2016
+www.epistemex.com
+MIT License (this header required)
+*/
+
+/**
+ * Creates a new undo-redo stack.
+ *
+ * @param {object} [options] - Optional option object (JSON)
+ * @param {number} [options.limit=-1] max number of entries. Stack will remove first (oldest) entry when limit is reached. Use -1 for unlimited number of entries.
+ * @param {function} [options.onUndo=null] optional callback function for undo(). Can also be set directly with the onundo property.
+ * @param {function} [options.onRedo=null] optional callback function for redo(). Can also be set directly with the onredo property.
+ * @constructor
+ */
+function UndoRedo(options) {
+
+	var me = this, n = null;			// just minimize magic
+
+	options = options || {};
+
+	me.lm = +(options.limit || -1);		// limit
+	me.st = [];							// stack array
+	me.sp = me.cn = 0;					// stack pointer / current index
+	me.fs = n;							// first out
+
+	/**
+	 * Set a function to call back when an undo is performed.
+	 * The argument given is the data for the state. If undo is not
+	 * possible it will be called with null as argument, so it can
+	 * be used to update button states etc.
+	 * @type {function|null}
+	 */
+	me.onundo = options.onUndo || n;
+
+	/**
+	 * Set a function to call back when an redo is performed.
+	 * The argument given is the data for the state. If redo is not
+	 * possible, the function will be called with null, so that
+	 * it can be used to update button states etc.
+	 * @type {function|null}
+	 */
+	me.onredo = options.onRedo || n;
+}
+
+UndoRedo.prototype = {
+
+	/**
+	 * Add data to the stack (push). Add the data you want to store as a state.
+	 * You can store any kind of data - the object passed will be stored
+	 * as-is. It returns the new stack pointer position.
+	 *
+	 * A call to add() will delete redo stack entries if any and reset
+	 * stack pointer to current state.
+	 *
+	 * @param {*} data - data object, number, array, ..., to store
+	 * @returns {number} - new stack pointer position
+	 */
+	add: function(data) {
+
+		var me = this,
+			stack = me.st,
+			pos = me.sp,
+			len = stack.length,
+			limit = me.lm;
+
+		if (pos < len)
+			stack.splice(pos, len - pos);
+
+		if (limit > -1 && pos === limit)
+			me.fs = stack.shift();
+
+		me.cn++;
+		stack.push(data);
+
+		return me.sp = stack.length
+	},
+
+	/**
+	 * Call this when you want to retrieve last state (pop). The data returned
+	 * can be used to replace current. If there are no undo states null
+	 * will be returned.
+	 *
+	 * You can call canUndo() in advance to check if undo is possible.
+	 *
+	 * @returns {*|null} Previous state object, or null if none
+	 */
+	undo: function() {
+
+		var me = this, res = null;
+
+		if (me.sp) {
+			me.cn--;
+			res =  me.st[--me.sp - 1] || me.fs;
+		}
+
+		if (me.onundo) me.onundo(res);
+		return res
+	},
+
+	/**
+	 * Redo can only be performed if you want to "cancel" a previous undo.
+	 * It will move the pointer forward and return the data representing
+	 * current state.
+	 *
+	 * If you call add() after a undo() call, redo is not possible.
+	 * You can use canRedo() to check if redo() is possible.
+	 *
+	 * @returns {*|null} current state to use, or null if redo is not possible.
+	 */
+	redo: function() {
+
+		var me = this, res = null, stack = me.st;
+
+		if (me.sp < stack.length) {
+			me.cn++;
+			res = stack[me.sp++];
+		}
+
+		if (me.onredo) me.onredo(res);
+		return res;
+	},
+
+	/**
+	 * This call will return true if undo() is possible. It can be used
+	 * to set status of a undo button for example.
+	 *
+	 * @returns {boolean}
+	 */
+	canUndo: function() {
+		return this.sp > 0;
+	},
+
+	/**
+	 * This call will return true if redo() is possible. It can be used
+	 * to set status of a redo button for example.
+	 *
+	 * @returns {boolean}
+	 */
+	canRedo: function() {
+		var len = this.st.length;
+		return len && this.sp < len
+	},
+
+	/**
+	 * Get current stack pointer/position in stack. Pointer is affected
+	 * by limit and will never exceed limit if limit is set. A pointer of
+	 * value 0 does not mean there hasn't been previous states, they can
+	 * simply have been purged due to the limit. Use count() to find
+	 * out if the stack would be in its initial state or not.
+	 * @returns {number}
+	 */
+	pointer: function() {
+		return this.sp
+	},
+
+	/**
+	 * Get total count track. Use this to find out if the stack is in its
+	 * original initial state, in which case 0 will be returned. If > 0
+	 * at first undo, it indicates that entries has been purged.
+	 * @returns {number}
+	 */
+	count: function() {
+		return this.cn
+	},
+
+	/**
+	 * Set or get current limit. If no argument is given then current
+	 * limit is returned. Provide a limit >= 1. If the new limit is lower
+	 * than existing entries, entries exceeding the limit will be purged.
+	 *
+	 * @param {number} [limit] - new limit
+	 * @returns {*|number} - if no argument is given current limit is returned.
+	 */
+	limit: function(limit) {
+
+		var me = this, len = me.st.length;
+
+		if (!limit) return me.lm;
+
+		me.lm = limit;
+
+		if (len > limit)
+			me.st.splice(limit, len);
+	},
+
+	/**
+	 * Get or set current stack. If no argument is given a stack object
+	 * is returned representing current stack. This object can for example
+	 * be stored (be sure data stored can be serialized if you intent to
+	 * use localStorage and similar mechanisms).
+	 *
+	 * Note that it references the entries internally.
+	 * The container object is to be considered private.
+	 *
+	 * @param {*} [stack] - a previously obtained stack object.
+	 * @returns {*} If no argument is given, a stack object is returned
+	 */
+	stack: function(stack) {
+		var me = this;
+
+		if (!stack) return {
+			st: me.st.concat(),
+			sp: me.sp
+		};
+
+		me.st = stack.st;
+		me.sp = +stack.sp;
+	}
+};
